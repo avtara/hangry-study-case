e291e6fcd84b547ae3e84311e0ca2d7735f5ddf5
avtara <avtarakhrisna1@gmail.com>
2023-12-27 21:33:00 +0700

    feat: setup openapi

---
+++ src/openapi/openapi.controller.ts
@@ -0,0 +1,16 @@
+import { Router } from 'express';
+import { Service } from 'typedi';
+import swaggerUi from 'swagger-ui-express';
+import doc from './spec.json';
+
+@Service()
+export class OpenAPIController {
+  routes() {
+    const router = Router();
+
+    router.use('/', swaggerUi.serve);
+    router.get('/', swaggerUi.setup(doc, { explorer: false }));
+
+    return router;
+  }
+}
