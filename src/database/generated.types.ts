60e399e3f15f1b88151b941ef308104a166eea51
avtara <avtarakhrisna1@gmail.com>
2023-12-28 09:35:30 +0700

    feat: init sql user

--- src/database/generated.types.ts
+++ src/database/generated.types.ts
@@ -1,1 +1,29 @@
-export interface DB {}
+import type { ColumnType } from "kysely";
+
+export type Generated<T> = T extends ColumnType<infer S, infer I, infer U>
+  ? ColumnType<S, I | undefined, U>
+  : ColumnType<T, T | undefined, T>;
+
+export type Timestamp = ColumnType<Date, Date | string, Date | string>;
+
+export type UserRole = "admin" | "guest";
+
+export interface Users {
+  created_at: Generated<Timestamp>;
+  created_by: Generated<string>;
+  deleted_at: Timestamp | null;
+  deleted_by: string | null;
+  email: string;
+  is_active: Generated<boolean>;
+  modified_at: Generated<Timestamp>;
+  modified_by: Generated<string>;
+  name: string;
+  password: string;
+  photo: string | null;
+  role: Generated<UserRole | null>;
+  user_id: Generated<number>;
+}
+
+export interface DB {
+  users: Users;
+}
--- src/database/schema.sql
+++ src/database/schema.sql
@@ -1,6 +1,23 @@
-CREATE DATABASE "hangry"
-    WITH
-    OWNER = postgres
-    ENCODING = 'UTF8'
-    CONNECTION LIMIT = -1
-    IS_TEMPLATE = False;
+-- CREATE DATABASE "hangry" WITH OWNER = postgres ENCODING = 'UTF8' CONNECTION
+-- LIMIT
+--     = -1 IS_TEMPLATE = False;
+
+CREATE TYPE user_role AS ENUM('guest', 'admin');
+
+CREATE TABLE IF NOT EXISTS users(
+    user_id serial PRIMARY KEY,
+    name VARCHAR (255) NOT NULL,
+    password VARCHAR (255) NOT NULL,
+    email VARCHAR (255) UNIQUE NOT NULL,
+    photo TEXT,
+    role user_role DEFAULT 'guest',
+    is_active BOOLEAN DEFAULT 'true' NOT NULL,
+    created_by VARCHAR(255) DEFAULT 'SYSTEM' :: CHARACTER VARYING NOT NULL,
+    created_at TIMESTAMP(0) DEFAULT NOW() NOT NULL,
+    modified_by VARCHAR(255) DEFAULT 'SYSTEM' :: CHARACTER VARYING NOT NULL,
+    modified_at TIMESTAMP(0) DEFAULT NOW() NOT NULL,
+    deleted_by VARCHAR(255),
+    deleted_at TIMESTAMP
+);
+CREATE INDEX idx_user ON users USING BTREE (user_id);
+
